0155e1f22ddbfb84687d311b393b3423e9120743
Godlove1 <ntangakalegodlove@gmail.com>
2024-11-15 12:33:00 +0100

    perms

--- src/app/(users)/layout.js
+++ src/app/(users)/layout.js
@@ -3,20 +3,23 @@ import { CartProviderContext } from "@/lib/CartProvider";
 import Header from "./landPageComps/Header";
 import Footer from "./landPageComps/Footer";
 import Chat from "@/components/custom/chat";
+import { AuthContextProvider } from "@/lib/Authcontext";
 
 export default function UserLayout({ children }) {
   return (
     <>
-      <CartProviderContext>
-        <Chat />
-        <div className="min-h-screen  flex flex-col">
-          <div className="flex-grow">
-            <Header />
-            {children}
+      <AuthContextProvider>
+        <CartProviderContext>
+          <Chat />
+          <div className="min-h-screen  flex flex-col">
+            <div className="flex-grow">
+              <Header />
+              {children}
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </CartProviderContext>
+        </CartProviderContext>
+      </AuthContextProvider>
     </>
   );
 }
